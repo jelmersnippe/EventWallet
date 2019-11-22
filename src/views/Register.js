0fcb3be5fd5e68cb60c62afe8d404c08853e7c60
0986130 <0986130@users.noreply.github.com>
2019-11-22 16:16:31 +0100

    Register 01
    Update to register

--- src/views/Register.js
+++ src/views/Register.js
@@ -3,9 +3,9 @@ import {
     View, 
     Text,
     StyleSheet,
-    TouchableOpacity
-} from 'react-native'
-
+    TouchableOpacity, 
+	TextInput,
+} from 'react-native';
 
 export default class Register extends Component {
     register () {
@@ -20,7 +20,20 @@ export default class Register extends Component {
                         this.register();
                     }}
                 >
-                    <Text>Register page</Text>
+                    <Text style={styles.header}>Registration</Text>
+
+                    <TextInput style={styles.textinput} placeholder="Username"
+                    underlineColorAndroid={'transparent'}/>
+
+                    <TextInput style={styles.textinput} placeholder="Password"
+                    secureTextEntry={true} underlineColorAndroid={'transparent'}/>
+
+                    <TextInput style={styles.textinput} placeholder="Email address"
+                    underlineColorAndroid={'transparent'}/>
+
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.bttntext}>Register</Text>
+                    </TouchableOpacity>
                 </TouchableOpacity>
             </View>
         );
@@ -30,9 +43,43 @@ export default class Register extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: 100+'%',
-    height: 100+'%',
+    paddingLeft: 50,
+    paddingRight: 50,
     justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: '#40e0d0'
   },
+  header: {
+    fontSize: 35,
+    color: 'white',
+    paddingBottom: 5,
+    marginBottom: 40,
+    borderBottomColor: 'white',
+    borderBottomWidth: 3,
+  },
+  textinput: {
+    alignSelf: 'stretch',
+    fontSize: 20,
+    height: 45,
+    marginBottom: 25,
+    color: 'white',
+    borderBottomColor: 'white',
+    borderBottomWidth: 2,
+    },
+   button: {
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#20b2aa',
+    marginTop: 20,
+    marginTop: 40,
+    paddingLeft: 60,
+    paddingRight: 60,
+    },
+   bttntext: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+    marginTop: 3,
+    marginBottom: 3,
+    }
+
 });
