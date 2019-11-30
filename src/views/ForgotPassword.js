6d89a27b0d49deb6731bccac14800f54a9cf5860
0986130 <0986130@users.noreply.github.com>
2019-11-30 17:03:49 +0100

    First draft password reset

--- src/views/ForgotPassword.js
+++ src/views/ForgotPassword.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react'
-import { 
-    View, 
+import {
+    View,
     Text,
     StyleSheet,
-    TouchableOpacity
-} from 'react-native'
+    TouchableOpacity,
+	TextInput,
+} from 'react-native';
 
-
-export default class Register extends Component {
+export default class ForgotPassword extends Component {
     passwordResetAction () {
-        // Make call to validate register attempt
+
+        // Make call to validate password reset attempt
     }
 
     render() {
         return (
             <View style={styles.container}>
-                <TouchableOpacity 
+                <Text style={styles.header}>Forgot password?</Text>
+
+                <Text style={styles.text}>Please enter your email address to recieve an email with a reset link.</Text>
+
+                <TextInput style={styles.textinput} placeholder="Email address"/>
+
+                <TouchableOpacity
                     onPress={() => {
                         this.passwordResetAction();
                     }}
+                    style={styles.button}
                 >
-                    <Text>Forgot password page</Text>
+                    <Text style={styles.bttntext}>Submit</Text>
                 </TouchableOpacity>
             </View>
         );
@@ -30,9 +38,51 @@ export default class Register extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: 100+'%',
-    height: 100+'%',
     justifyContent: 'center',
-    alignItems: 'center'
   },
+  header: {
+    fontSize: 25,
+    color: 'black',
+    marginTop: 30,
+    paddingLeft: 50,
+    paddingRight: 50,
+  },
+  text: {
+    fontSize: 15,
+    color: 'black',
+    marginTop: 30,
+    marginBottom: 30,
+    paddingLeft: 50,
+    paddingRight: 50,
+  },
+  textinput: {
+    fontSize: 20,
+    height: 45,
+    marginBottom: 25,
+    color: 'black',
+    backgroundColor: 'gray',
+    paddingLeft: 10,
+    marginLeft: 50,
+    marginRight: 50,
+    },
+   button: {
+    alignItems: 'center',
+    backgroundColor: 'black',
+    marginTop: 20,
+    marginBottom: 20,
+    paddingLeft: 30,
+    paddingRight: 30,
+    paddingBottom: 10,
+    paddingTop: 10,
+    marginLeft: 100,
+    marginRight: 100,
+    },
+   bttntext: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+    marginTop: 3,
+    marginBottom: 3,
+    }
+
 });
