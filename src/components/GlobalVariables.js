8393262d9deffff5707ec2cbecf18260b4b260a9
09026722 <09026722@hr.nl>
2019-12-12 14:22:57 +0100

    Added file for global styling

---
+++ src/components/GlobalVariables.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native'
+
+export const Colors = {
+    eventColor: '#F6CF3A',
+    friendColor: '#0070C0',
+}
+
+export const Font = {
+    mainFont: 'Arial'
+}
+
+// export const padded_container = StyleSheet.create({
+//     flex: 1,
+//     paddingHorizontal: 3+'%'
+// })
